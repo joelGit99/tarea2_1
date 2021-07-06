5b5dcaecc354cb7765ca315e97bffab986275d25
Joel <Joel@DESKTOP-T023OHQ>
2021-07-06 13:33:35 +0200

    Tarea2_1

---
+++ Tarea2_1/src/Tarea2_1.java
@@ -0,0 +1,10 @@
+
+public class Tarea2_1 {
+
+	public static void main(String[] args) {
+		//System.out.println("Hola");
+		System.out.println("Adios");
+		System.out.println("FIN DE PROGRAMA");
+	}
+
+}
